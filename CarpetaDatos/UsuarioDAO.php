b8856607612010650ef140782b5d0ddba4f8777e
Nessy <vaneee10@gmail.com>
2020-10-28 14:21:16 +0100

    guardar usuario a medias

--- CarpetaDatos/UsuarioDAO.php
+++ CarpetaDatos/UsuarioDAO.php
@@ -2,13 +2,12 @@
     include_once "../Carpetamodelo/Usuario.php";
     $login= 'admin';
     $password='1234'; 
-    $usu = new UsuarioDAO();
+    $usu = new Usuario(5, 'paquito', 'gerlo', 1 );
     $usuDAO = new UsuarioDAO();//creamos un objeto para poder utilizar las funciones de dentro de la clase
     $usuDAO-> obtenerUsuario($login, $password);
-    //$usuDAO-> guardarUsuario ($usu);
+    $usuDAO-> guardarUsuario ($usu);
 
-    class UsuarioDAO{
-        
+    class UsuarioDAO{      
         function crearConexion(){//conexion de php a la bbdd
             $servidorBD = 'localhost';
             $usuarioBD = 'root';
@@ -22,7 +21,7 @@
         }
         
         function obtenerUsuario ($login, $password){
-            $conexion = $this -> crearConexion();//peta
+            $conexion = $this -> crearConexion();
             $sql = "SELECT id, login, password, alumno_id FROM  USUARIO WHERE login=? and password=?;";
             $consultaPreparada=$conexion->prepare ($sql);
             $consultaPreparada-> bind_param("ss", $login, $password);
@@ -34,22 +33,38 @@
         }
 
         function guardarUsuario ($usuario){
-            $conexion= $this-> crearConexion();
-          
             $login = $usuario->getLogin();
             $password = $usuario->getPassword();
             $alumno_id = $usuario -> getAlumno_id();
-            $sql = "INSERT INTO USUARIO( id, login, password, alumno_id) values (?, ?, ?, ?);";
-            $consultaPreparada=$conexion->prepare ($sql);
-            //asignariamos valores a los campos
-            $consultaPreparada = bind_param("ssi", $login, $password, $alumno_id);
-            $consultaPreparada = execute();
-            $id = $conexion->insert_id;
-            var_dump($id);
-            $consultaPreparada = close();
+            $conexion= $this-> crearConexion(); 
+            $id = 5;//prueba para ver si cambia
+            if ($id!=0){// si tiene id -> 0  hacemos update || id no esta definido   
+                $id = $usuario-> setId(5);  //No me cambia el id   //no me hace update
+                $sql = "UPDATE USUARIO SET id='$id' ,login='$login', password='$password', alumno_id='$alumno_id' WHERE id='$id';";
+                $consultaPreparada=$conexion->prepare($sql);//le he puesto un i a bind_param error por demasiados parametros 
+                $consultaPreparada->bind_param("issi", $id, $login, $password, $alumno_id);//error por poner line 41->YA NO
+                $consultaPreparada-> execute();
+                $id = $conexion->insert_id;
+                var_dump($id);
+                //SET login = ? o como 
+
+            }else{//Insertamos
+                $sql = "INSERT INTO USUARIO( login, password, alumno_id) values ( ?, ?, ?);";//autoincrementales no se pasan
+                $consultaPreparada=$conexion->prepare ($sql);
+                //asignariamos valores a los campos
+                $consultaPreparada ->bind_param("ssi", $login, $password, $alumno_id);
+                $consultaPreparada -> execute();
+                $id = $conexion->insert_id;
+                var_dump($id);
+            }
+            $conexion->close();
         }
 
         function eliminarUsuario ($usuario){
+            $conexion= $this-> crearConexion();
+            $id = $usuario->getId();
+            $sql = "SELECT id, login, password, alumno_id FROM  USUARIO WHERE login=? and password=?;";
+            $sql = "DELETE FROM USUARIO( login, password, alumno_id) where (id=?);";
 
         }
 
