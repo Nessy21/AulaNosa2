7be30eaaa00464cb12240e9c67d1dc7b2f68b7bc
Nessy <vaneee10@gmail.com>
2020-11-09 10:59:00 +0100

    nada

--- CarpetaModelo/ServicioAlumnos.php
+++ CarpetaModelo/ServicioAlumnos.php
@@ -6,20 +6,23 @@
 
     
     function obtenerListadoAlumnos (){//retorna todos los Alumnos de la bbdd
-        $conexion = $this -> crearConexion();//
+        $conexion = $this -> crearConexion();// error
         $sql = "SELECT id, nombre, apellidos, fecha_nacimiento FROM  ALUMNO ;";
         $consultaPreparada=$conexion->prepare($sql);
         $consultaPreparada->execute();
         $resultado = $consultaPreparada->get_result();
-        $filas = $resultado->fetch_array();
-            //$al = new Alumno ($fila[1],...);
-            //$listaAlumnos[i] = $al;
+        $filas = $resultado->fetch_array(); //fetch array devuelve array o falso
         $i=0;
-        while ($filas!=false){
-            $filas = $resultado->fetch_array();//devuelve un array con los resultados de la fila
+        var_dump($filas);
+        do{//$filas != false
+            //devuelve un array con los resultados de la fila
             $listaAlumnos [$i];
             $i++;
-            }
+            var_dump($listaAlumnos[$i]);
+            //$al = new Alumno ($fila[1],...);
+            //$listaAlumnos[i] = $al;
+        }
+        while ($filas = $resultado->fetch_array());     
         $conexion->close();
 
             
