bb49055577c28dd84c0920776ca2e6d5a9e43370
Nessy <vaneee10@gmail.com>
2020-11-16 08:52:50 +0100

    .

--- CarpetaDatos/AlumnoDAO.php
+++ CarpetaDatos/AlumnoDAO.php
@@ -76,6 +76,31 @@
             $conexion->close();
         }
     
-    }
+        function listadoAlumnos(){
+            $conexion=$this->crearConexion();
+            $sql="SELECT id, nombre, apellidos, fecha_nacimiento FROM  ALUMNO ;";
+            $consultaPreparada=$conexion->prepare($sql);
+            $consultaPreparada->execute();
+            $resultado=$consultaPreparada->get_result();
+            $filas=$resultado->fetch_array(); //fetch array devuelve array o falso
+            $i=0;
+            var_dump($filas);
+            do{//$filas != false
+                //devuelve un array con los resultados de la fila
+                $listaAlumnos [$i];
+                $i++;
+                var_dump($listaAlumnos[$i]);
+                //$al = new Alumno ($fila[1],...);
+                //$listaAlumnos[i] = $al;
+            }
+            while ($filas = $resultado->fetch_array());     
+            $conexion->close();
+        }
 
+
+
+
+
+
+    }//class
 ?>
--- CarpetaModelo/ServicioAlumnos.php
+++ CarpetaModelo/ServicioAlumnos.php
@@ -1,40 +1,38 @@
 <?php
-    include_once "../CarpetaDatos/AlumnoDAO.php";
+    include_once "../CarpetaDatos/AlumnoDAO.php";//intermediario entre los datos y la vista ><
    obtenerListadoAlumnos();
-    //guardarAlumno() 
-    $al = new Alumno ();//meterle parámetros 
+    //guardarAlumno()
+    $al = new Alumno ();//meterle parámetros
 
-    
-    function obtenerListadoAlumnos (){//retorna todos los Alumnos de la bbdd
-        $conexion = $this -> crearConexion();// error
-        $sql = "SELECT id, nombre, apellidos, fecha_nacimiento FROM  ALUMNO ;";
-        $consultaPreparada=$conexion->prepare($sql);
-        $consultaPreparada->execute();
-        $resultado = $consultaPreparada->get_result();
-        $filas = $resultado->fetch_array(); //fetch array devuelve array o falso
-        $i=0;
-        var_dump($filas);
-        do{//$filas != false
-            //devuelve un array con los resultados de la fila
-            $listaAlumnos [$i];
-            $i++;
-            var_dump($listaAlumnos[$i]);
-            //$al = new Alumno ($fila[1],...);
-            //$listaAlumnos[i] = $al;
-        }
-        while ($filas = $resultado->fetch_array());     
-        $conexion->close();
 
-            
+    function obtenerListadoAlumnos(){//retorna todos los Alumnos de la bbdd
+        // $conexion=$this->crearConexion();// error
+        // $sql = "SELECT id, nombre, apellidos, fecha_nacimiento FROM  ALUMNO ;";
+        // $consultaPreparada=$conexion->prepare($sql);
+        // $consultaPreparada->execute();
+        // $resultado = $consultaPreparada->get_result();
+        // $filas = $resultado->fetch_array(); //fetch array devuelve array o falso
+        // $i=0;
+        // var_dump($filas);
+        // do{//$filas != false
+        //     //devuelve un array con los resultados de la fila
+        //     $listaAlumnos [$i];
+        //     $i++;
+        //     var_dump($listaAlumnos[$i]);
+        //     //$al = new Alumno ($fila[1],...);
+        //     //$listaAlumnos[i] = $al;
+        // }
+        // while ($filas = $resultado->fetch_array());
+        // $conexion->close();
     }
 
     function guardarAlumno ($alumno){
         //recibe tipo alumno y crea en bbdd mediante el metodo del dao
         guardarAlumno($alumno);
 
-        
+
     }
-    
+
 
 
 ?>
